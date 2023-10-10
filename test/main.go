7f5563294e025efa4fb72b2c4e84ebe1a0ca7549
olgaevstratova <olenkaevstratova@gmail.com>
2023-10-10 16:37:35 +0300

    Исправлен модуль ConverterPoundsToKilograms

--- test/main.go
+++ test/main.go
@@ -1,8 +1,23 @@
 package main
 
+import "math"
+
 func ConvertPoundsToKilograms(pounds float64) float64 {
 	kilograms := pounds * 0.45359237
-	return kilograms
+	if pounds < 0 {
+		kilograms = 0.0
+	}
+	var rounder float64
+	pow := math.Pow(10, float64(3))
+	intermed := kilograms * pow
+	_, frac := math.Modf(intermed)
+	if frac >= 0.5 {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow
 }
 func ConvertFeetToMeters(feet float64) float64 {
 	return 0
