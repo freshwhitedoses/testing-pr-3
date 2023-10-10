a054881e08eac831130f7c64fc3368328db78fa2
olgaevstratova <olenkaevstratova@gmail.com>
2023-10-10 16:26:14 +0300

    Написан модуль ConverterPoundsToKilograms

--- test/main.go
+++ test/main.go
@@ -1,7 +1,8 @@
 package main
 
 func ConvertPoundsToKilograms(pounds float64) float64 {
-	return 0
+	kilograms := pounds * 0.45359237
+	return kilograms
 }
 func ConvertFeetToMeters(feet float64) float64 {
 	return 0
--- test/main_test.go
+++ test/main_test.go
@@ -5,41 +5,41 @@ import (
 	"testing"
 )
 
-func TestingTestConverterPoundsToKilogramsNull(t *testing.T) {
+func TestConverterPoundsToKilogramsNull(t *testing.T) {
 	mean := ConvertPoundsToKilograms(0)
 	assert.Equal(t, 0.0, mean, "Неверное определение килограмм для нулевого значения")
 }
-func TestingTestConverterPoundsToKilogramsSimple(t *testing.T) {
+func TestConverterPoundsToKilogramsSimple(t *testing.T) {
 	mean := ConvertPoundsToKilograms(22)
 	assert.Equal(t, 9.979, mean, "Неверное определение килограмм для значения в фунтах")
 }
-func TestingTestConverterPoundsToKilogramsMinusVar(t *testing.T) {
+func TestConverterPoundsToKilogramsMinusVar(t *testing.T) {
 	mean := ConvertPoundsToKilograms(-30)
 	assert.Equal(t, 0.0, mean, "Неверное определение килограмм для отрицательного значения")
 }
 
-func TestingTestConvertFeetToMetersNull(t *testing.T) {
+func TestTestConvertFeetToMetersNull(t *testing.T) {
 	mean := ConvertFeetToMeters(0)
 	assert.Equal(t, 0.0, mean, "Неверное определение метров для нулевого значения")
 }
-func TestingConvertFeetToMetersSimple(t *testing.T) {
+func TestConvertFeetToMetersSimple(t *testing.T) {
 	mean := ConvertFeetToMeters(5)
 	assert.Equal(t, 1.524, mean, "Неверное определение метров для значения в футов")
 }
-func TestingTestConvertFeetToMetersMinusVar(t *testing.T) {
+func TestTestConvertFeetToMetersMinusVar(t *testing.T) {
 	mean := ConvertFeetToMeters(-30)
 	assert.Equal(t, 0.0, mean, "Неверное определение метров для отрицательного значения")
 }
 
-func TestingTestConvertFahrenheitToCelsiusNull(t *testing.T) {
+func TestConvertFahrenheitToCelsiusNull(t *testing.T) {
 	mean := ConvertFahrenheitToCelsius(0)
 	assert.Equal(t, -17.778, mean, "Неверное определение цельсий для нулевого значения")
 }
-func TestingConvertFahrenheitToCelsiusSimple(t *testing.T) {
+func TestConvertFahrenheitToCelsiusSimple(t *testing.T) {
 	mean := ConvertFeetToMeters(112)
 	assert.Equal(t, 44.444, mean, "Неправильное определение цельсий для значения в фаренгейтах")
 }
-func TestingTestConvertFahrenheitToCelsiusMinusVar(t *testing.T) {
+func TestTestConvertFahrenheitToCelsiusMinusVar(t *testing.T) {
 	mean := ConvertFeetToMeters(-30)
 	assert.Equal(t, -34.445, mean, "Неверное определение цельсий для отрицательного значения")
 }
